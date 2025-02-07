4f8e03c1897fabbea8535716ed6f03d3e03760df
Rehan Nagoor <rehannags@yahoo.com>
2025-02-07 17:47:26 +0400

    init next app

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Qiraathub Academy",
+  description: "Learn the 10 Qiraat with Qiraathub Academy",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div>
+      <div className="text-center max-w-2xl mx-auto pt-[5rem] px-4">
+        <h1 className="text-4xl font-bold mb-6 relative inline-block">
+          Learn the Ten Qiraat
+          <div className="absolute right-[-10px] bottom-[-5px] w-[190px] h-[3px] bg-yellow-400 rounded-full"></div>
+        </h1>
+        <p className="text-lg text-gray-700 pt-6">
+          The ten Qiraat are distinct styles of Quranic recitation, each preserving the authenticity and beauty of the Quran.
+        </p>
+      </div>
+    </div>
+  );
+}
